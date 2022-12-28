bfd07fac8203eda4c86a0b9320c4a498a1310e8b
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-28 23:05:18 +0800

    Initial commit

--- RickAndMortyAPI/ViewController.swift
+++ RickAndMortyAPI/ViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow        
     }
-
-
 }
 
