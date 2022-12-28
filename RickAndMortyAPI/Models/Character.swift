5bd27d8adf9996ac66cc69f31aae301250ece65b
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-29 00:04:03 +0800

    Finalize models for API request

--- RickAndMortyAPI/Models/Character.swift
+++ RickAndMortyAPI/Models/Character.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+struct Character: Codable {
+    
+    let id: Int?
+    let name: String?
+    let status: Status?
+    let species: String?
+    let type: String?
+    let gender: Gender?
+    let origin: Origin?
+    let location: SingleLocation?
+    let image: String?
+    let episode: [String]?
+    let url: String?
+    let created: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case status = "status"
+        case species = "species"
+        case type = "type"
+        case gender = "gender"
+        case origin = "origin"
+        case location = "location"
+        case image = "image"
+        case episode = "episode"
+        case url = "url"
+        case created = "created"
+    }
+}
+
+struct Origin: Codable {
+    
+    let name: String
+    let url: String
+}
+
+struct SingleLocation: Codable {
+    
+    let name: String
+    let url: String
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "Unknown"
+}
+
+enum Gender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "Unknown"
+}
--- RickAndMortyAPI/Models/Episode.swift
+++ RickAndMortyAPI/Models/Episode.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Episode: Codable {
+    
+    let id: Int?
+    let name: String?
+    let airDate: String?
+    let episode: String?
+    let characters: [String]?
+    let url: String?
+    let created: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case airDate = "air_date"
+        case episode = "episode"
+        case characters = "characters"
+        case url = "url"
+        case created = "created"
+    }
+}
