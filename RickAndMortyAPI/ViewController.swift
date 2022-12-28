59762962db266f694f71e324997e351c113c66a8
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-28 22:59:14 +0800

    Initial Commit

---
+++ RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Stephen Giovanni Saputra on 28/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
