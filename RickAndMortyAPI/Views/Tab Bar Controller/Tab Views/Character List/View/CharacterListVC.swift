b3b2e7cd1fb86c304414014eff764067a56f1299
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-31 11:00:53 +0800

    Move View to VC for saving code

--- RickAndMortyAPI/Views/Tab Bar Controller/Tab Views/Character List/View/CharacterListVC.swift
+++ RickAndMortyAPI/Views/Tab Bar Controller/Tab Views/Character List/View/CharacterListVC.swift
@@ -11,13 +11,44 @@ import UIKit
 final class CharacterListVC: UIViewController {
 
     // MARK: - Properties
-    private let characterListView = CharacterListView()
+    internal lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.style = .medium
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    internal lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        return layout
+    }()
+    
+    internal lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.register(CharacterCollectionViewCell.self, forCellWithReuseIdentifier: CharacterCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    private let viewModel = CharacterListVM()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         
         super.viewDidLoad()
         configureUI()
+        
+        spinner.startAnimating()
+        
+        viewModel.fetchCharacters()
+        viewModel.delegate = self
     }
     
     // MARK: - Selectors
@@ -29,12 +60,68 @@ final class CharacterListVC: UIViewController {
         self.title = "Characters"
         view.backgroundColor = .systemBackground
         
-        view.addSubview(characterListView)
+        view.addSubviews(collectionView, spinner)
         NSLayoutConstraint.activate([
-            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+            
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor)
         ])
+    }
+}
+
+extension CharacterListVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.viewModel.characters.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCollectionViewCell.identifier, for: indexPath) as? CharacterCollectionViewCell else {
+            fatalError("Unsupported cell")
+        }
+
+        let vm = CharacterCollectionViewCellVM(
+            characterName: self.viewModel.characters[indexPath.row].name ?? "",
+            characterStatus: self.viewModel.characters[indexPath.row].status ?? .unknown,
+            characterImageURL: URL(string: self.viewModel.characters[indexPath.row].image ?? "")
+        )
+        cell.configure(with: vm)
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width - 45) / 2
+
+        return CGSize(width: width, height: width * 1.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print(String(describing: self.viewModel.characters[indexPath.row].image ?? ""))
+    }
+}
+
+extension CharacterListVC: CharacterListVMDelegate {
+    
+    func didLoadInitialCharacters() {
+        
+        spinner.stopAnimating()
+        collectionView.reloadData()
+        
+        collectionView.isHidden = false
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
     }
 }
